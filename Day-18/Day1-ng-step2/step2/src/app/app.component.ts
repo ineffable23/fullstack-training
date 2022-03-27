4a7bd04b968bc721c67e2de234833ea3c4c8b085
ineffable23 <49369387+ineffable23@users.noreply.github.com>
2022-03-28 02:40:02 +0530

    ng step 2

--- Day-18/Day1-ng-step2/step2/src/app/app.component.ts
+++ Day-18/Day1-ng-step2/step2/src/app/app.component.ts
@@ -2,9 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Binding 101</h1>
+    <hr>
+    <h2>{{ title.length }}</h2>       <!-- interpolation -->
+    <h2 innerHTML="{{ title }}"></h2>        <!-- //attribute binding -->
+    <h2 innerText="{{ title }}"></h2>        <!-- //attribute binding -->
+    <h2 [innerText]="title"></h2>            <!-- //attribute binding -->
+    <h2 bind-innerText="title"></h2> 
+    <h3 [class]="ischool">Can I be a red box</h3>
+    <input type="text" [value]="title">
+  `,
+  styles : [`
+    .redbox{
+      width : 400px;
+      height : 100px;
+      border : 2px solid red;
+      padding : 10px;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'step2';
+  title = 'This is my step2 project in Angular';
+  ischool = "redbox";
 }
