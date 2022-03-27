5f1e77b89efa41425b364311e2187e07631c99ff
ineffable23 <49369387+ineffable23@users.noreply.github.com>
2022-03-28 00:56:05 +0530

    Day 18

---
+++ Day-18/Day1-ng-steps/steps/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <h1>Hello Angular</h1>
+  `
+})
+export class AppComponent {
+  title = 'steps';
+}
