dc861668197757cd4b24bc61b8e593c69e6ad7bb
ineffable23 <49369387+ineffable23@users.noreply.github.com>
2022-04-06 19:03:28 +0530

    add movie and add hero

--- Day-24/steps/projects/step4-featured-routes/src/app/heroadd.component.ts
+++ Day-24/steps/projects/step4-featured-routes/src/app/heroadd.component.ts
@@ -1,19 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from './herodetails.service';
 
 @Component({
   selector: 'app-heroadd',
   template: `
-    <p>
-      heroadd works!
-    </p>
+    <div class="container">
+  <div class="mb-3">
+    <label for="name" class="form-label">Enter Hero New Name</label><br>
+    <input type="text" name="hname" id="name" [(ngModel)]="addHero.name" class="form-control">
+    </div>
+    <label class="form-label">Edit Hero Power Status</label>
+    <div class="mb-3">
+    <label for="intelligence" class="form-label">Intelligence Level</label><br>
+    <input type="range" min=0 max=100 id="intelligence" [(ngModel)]="addHero.powerstats.intelligence" class="form-range" ></div>
+   <div class="mb-3">
+    <label for="strength" class="form-label">Strength</label><br>
+    <input type="range" min=0 max=100 id="strength" [(ngModel)]="addHero.powerstats.strength" class="form-range" >
+    </div>
+    <div class="mb-3">
+    <label for="speed" class="form-label">Speed</label><br>
+    <input type="range" min=0 max=100 id="speed" [(ngModel)]="addHero.powerstats.speed" class="form-range" >
+    </div>
+    <div class="mb-3">
+    <label for="durability" class="form-label">Durability</label><br>
+    <input type="range" min=0 max=100 id="durability" [(ngModel)]="addHero.powerstats.durability" class="form-range" >
+    </div>
+    <div class="mb-3">
+    <label for="power" class="form-label">Power</label><br>
+    <input type="range" min=0 max=100 id="power" [(ngModel)]="addHero.powerstats.power" class="form-range" >
+    </div>
+    <div class="mb-3">
+    <label for="combat" class="form-label">Combat</label><br>
+    <input type="range" min=0 max=100 id="combat" [(ngModel)]="addHero.powerstats.combat" class="form-range" >
+    </div>
+    <label class="form-label">Edit Hero Biography</label>
+    <div class="mb-3">
+    <label for="fullname" class="form-label">New Character Name</label><br>
+    <input type="text" id="fullname" [(ngModel)]="addHero.biography['full-name']" class="form-control" ></div>
+   <div class="mb-3">
+    <label for="pob" class="form-label">Character Place of Birth</label><br>
+    <input type="text" id="pob" [(ngModel)]="addHero.biography['place-of-birth']" class="form-control" >
+    </div>
+    <div class="mb-3">
+    <label for="publisher" class="form-label">Edit Publisher</label><br>
+    <input type="text" id="publisher" [(ngModel)]="addHero.biography['publisher']" class="form-control" >
+    </div>
+    <div class="mb-3">
+    <label for="release" class="form-label">Release Date</label><br>
+    <input type="date" id="release" [(ngModel)]="addHero.biography['release-date']" class="form-control" >
+    </div>
+    <button><a [routerLink]="['/heroes']">Add Hero</a></button>
+    </div>
   `,
   styles: []
 })
 export class HeroaddComponent implements OnInit {
-
-  constructor() { }
+  addHero:any;
+  constructor(private hs:HeroService, private ar:ActivatedRoute) { }
 
   ngOnInit() {
+    this.addHero = this.hs.getSelectedHero(this.ar.snapshot.queryParams['heroid']);
   }
 
 }
--- Day-24/steps/projects/step4-featured-routes/src/app/movieadd.component.ts
+++ Day-24/steps/projects/step4-featured-routes/src/app/movieadd.component.ts
@@ -1,19 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from './moviedetails.service';
 
 @Component({
-  selector: 'app-movieadd',
+  selector: 'app-movieedit',
   template: `
-    <p>
-      movieadd works!
-    </p>
+    <div class="container">
+    <div class="mb-3">
+    <label for="mtitle" class="form-label">New Movie Name</label><br>
+    <input type="text" id="mtitle" [(ngModel)]="addMovie.title" class="form-control" ></div>
+   <div class="mb-3">
+    <label for="year" class="form-label">Release Year</label><br>
+    <input type="text" id="year" [(ngModel)]="addMovie.year" class="form-control" >
+    </div>
+    <div class="mb-3">
+    <label for="director" class="form-label">Edit Director</label><br>
+    <input type="text" id="director" [(ngModel)]="addMovie.director" class="form-control" >
+    </div>
+    <div class="mb-3">
+    <label for="Genres" class="form-label">Genres</label><br>
+    <input type="text" id="Genres" [(ngModel)]="addMovie.genres" class="form-control" >
+    </div>
+    <div class="mb-3">
+    <label for="actors" class="form-label">Movie Actors</label><br>
+    <input type="text" id="actors" [(ngModel)]="addMovie.actors" class="form-control" >
+    </div>
+    <button><a [routerLink]="['/movies']">Add Movie</a></button>
+    </div>
   `,
   styles: []
 })
 export class MovieaddComponent implements OnInit {
 
-  constructor() { }
+  addMovie:any;
+  constructor(private hr:MovieService, private ar:ActivatedRoute) { }
 
   ngOnInit() {
+    this.addMovie= this.hr.getSelectedMovie(this.ar.snapshot.queryParams['mid']);
   }
 
 }
